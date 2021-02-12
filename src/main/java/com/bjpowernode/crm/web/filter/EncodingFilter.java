190417f96553e57f61bd87a8d2e496c38a9e2ec7
zs <zs@bjpowernode.com>
2021-02-12 18:59:56 +0800

    去去去

---
+++ src/main/java/com/bjpowernode/crm/web/filter/EncodingFilter.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.crm.web.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("进入到字符编码过滤器");
+        servletRequest.setCharacterEncoding("utf-8");
+        servletResponse.setContentType("text/html;charset=utf-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
