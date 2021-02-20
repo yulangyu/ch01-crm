424aff8573bfe8693b01e460ba8f6b6e7fe96671
zs <zs@bjpowernode.com>
2021-02-20 22:16:38 +0800

    去去去

---
+++ src/main/java/com/bjpowernode/crm/settings/domain/DicType.java
@@ -0,0 +1,36 @@
+package com.bjpowernode.crm.settings.domain;
+
+/**
+ * @author: yulangyu
+ * @date: 2021/2/20 22:13
+ * @version: 1.0
+ */
+public class DicType {
+    private String code;
+    private String name;
+    private String description;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/bjpowernode/crm/workbench/web/controller/ClueController.java
@@ -0,0 +1,35 @@
+package com.bjpowernode.crm.workbench.web.controller;
+
+import com.bjpowernode.crm.settings.domain.User;
+import com.bjpowernode.crm.settings.service.UserService;
+import com.bjpowernode.crm.settings.service.impl.UserServiceImpl;
+import com.bjpowernode.crm.utils.DateTimeUtil;
+import com.bjpowernode.crm.utils.PrintJson;
+import com.bjpowernode.crm.utils.ServiceFactory;
+import com.bjpowernode.crm.utils.UUIDUtil;
+import com.bjpowernode.crm.vo.PagenationVo;
+import com.bjpowernode.crm.workbench.domain.Activity;
+import com.bjpowernode.crm.workbench.domain.ActivityRemark;
+import com.bjpowernode.crm.workbench.service.ActivityService;
+import com.bjpowernode.crm.workbench.service.impl.ActivityServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClueController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("进入到市场控制器");
+        String path =request.getServletPath();
+        if ("/workbench/activity/getUserList.do".equals(path)){
+            getUserList(request,response);
+        }
+    }
+
+}
